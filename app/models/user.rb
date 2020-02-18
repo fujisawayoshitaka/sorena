d0493069f2ce8c3c9a2533242e7eda50c48c7ddc
fujisawayoshitaka <1317699@gmail.com>
2020-02-18 13:48:29 +0900

    管理者user機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         validates :name, presence: true
+         validates :name, presence: true, length: { maximum: 20 }
          validates :profile, length: { maximum: 200 }
          has_many :netas, dependent: :destroy
          has_many :favorites, dependent: :destroy
@@ -9,4 +9,12 @@ class User < ApplicationRecord
          has_many :comments, dependent: :destroy
          has_many :station_user, dependent: :destroy
          has_many :stations, through: :station_user
+
+         before_destroy do
+           throw(:abort) if User.where(admin: true).count <= 1 && self.admin?
+         end
+
+         before_update do
+           throw(:abort) if User.where(admin: true).count <= 1 && self.admin?
+         end
 end
